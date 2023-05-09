02afe62a902a31b464d2d61f9c30b777e5d2d54d
Some-O-ne <70059895+Some-O-ne@users.noreply.github.com>
2023-05-09 21:46:44 +0300

    Update server.py

--- ircreinvent_noway/server.py
+++ ircreinvent_noway/server.py
@@ -7,17 +7,24 @@ def handle_connection(connection, address,members_socks):
         msg = connection.recv(BUF).decode()
         if not msg:
             continue
+        if msg[0:3] == "!j ":
+            members[address] = msg[3:]
+            for connect in members_socks:
+                connect.send(f"{msg[3:]} has entered the chat!".encode())
+            continue
         for connect in members_socks:
-            connect.send(f"{str(address)}: {msg}".encode())
+            connect.send(f"{str(members[address])}: {msg}".encode())
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 port = input("Server Port: ")
 sock.bind(("127.0.0.1", int(port)))
 sock.listen()
 members_socks = []
+members = {}
 while True:
     connection, address = sock.accept()
     members_socks.append( connection)
+
     threading.Thread(
         target = handle_connection,
         args = (connection, address,members_socks),
