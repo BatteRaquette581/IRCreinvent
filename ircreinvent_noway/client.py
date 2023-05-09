e2b75dfe2084f36b5b923ecad0de6131e0a2038e
Some-O-ne <70059895+Some-O-ne@users.noreply.github.com>
2023-05-09 21:46:25 +0300

    Update client.py

--- ircreinvent_noway/client.py
+++ ircreinvent_noway/client.py
@@ -2,19 +2,20 @@ import socket
 import threading
 def send_msg(conn):
     while True:
-        msg = input(">>>")
+        msg = input()
         conn.send(msg.encode())
 def wait_msg(conn):
     while True:
         msg = conn.recv(1024).decode()
-        print(msg,end="")
+        print(msg,end="\n")
 s = socket.socket()
 port = input("Server Port: ")
 hs = input("Server Host Name: ")
+us = input("Username: ")
 s.connect((hs,int(port)))
+s.send(f"!j {us}".encode())
 threading.Thread(target=send_msg,args=(s,)).start()
 threading.Thread(target=wait_msg,args=(s,)).start()
-
 
 
 
