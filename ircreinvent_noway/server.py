71b1dc35ee58af4385aed2d9ea4cb800c58a2331
Some-O-ne <70059895+Some-O-ne@users.noreply.github.com>
2023-05-09 20:11:56 +0300

    It actually works :skull:

--- ircreinvent_noway/server.py
+++ ircreinvent_noway/server.py
@@ -1,22 +1,25 @@
 import socket
 import threading
 
-def handle_connection(connection, address):
+def handle_connection(connection, address,members_socks):
     BUF = 1024
     while True:
         msg = connection.recv(BUF).decode()
         if not msg:
             continue
-        connection.send(f"{str(address)}: {msg}").encode()
+        for connect in members_socks:
+            connect.send(f"{str(address)}: {msg}".encode())
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 port = input("Server Port: ")
 sock.bind(("127.0.0.1", int(port)))
 sock.listen()
+members_socks = []
 while True:
     connection, address = sock.accept()
+    members_socks.append( connection)
     threading.Thread(
         target = handle_connection,
-        args = (connection, address),
+        args = (connection, address,members_socks),
         daemon = True
     ).start()
