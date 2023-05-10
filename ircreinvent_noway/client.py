0aad2751c5668a6c0fa8b1f41203831a99927220
Some-O-ne <70059895+Some-O-ne@users.noreply.github.com>
2023-05-10 20:56:10 +0300

    Update client.py + made it PEP

--- ircreinvent_noway/client.py
+++ ircreinvent_noway/client.py
@@ -1,18 +1,24 @@
 import socket
 import threading
+
+
 def send_msg(conn):
     while True:
         msg = input()
         conn.send(msg.encode())
+
+
 def wait_msg(conn):
     while True:
         msg = conn.recv(1024).decode()
-        print(msg, end = "\n")
+        print(msg, end="\n")
+
+
 s = socket.socket()
 port = input("Server Port: ")
 hs = input("Server Host Name: ")
 us = input("Username: ")
 s.connect((hs, int(port)))
-s.send(f"!j {us}".encode())
-threading.Thread(target = send_msg, args = (s,)).start()
-threading.Thread(target = wait_msg, args = (s,)).start()
+s.send(f"{us}".encode())
+threading.Thread(target=send_msg, args=(s,)).start()
+threading.Thread(target=wait_msg, args=(s,)).start()
