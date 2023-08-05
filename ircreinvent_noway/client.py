89c17e4c17653e989b23f101187fc118515757fe
BatteRaquette581 <84451047+BatteRaquette581@users.noreply.github.com>
2023-08-05 12:38:49 +0200

    Add files via upload

--- ircreinvent_noway/client.py
+++ ircreinvent_noway/client.py
@@ -1,24 +1,27 @@
 import socket
 import threading
 
+running = True
 
 def send_msg(conn):
-    while True:
+    global running
+    while running:
         msg = input()
         conn.send(msg.encode())
-
+        if msg == "!exit":
+            running = False
 
 def wait_msg(conn):
-    while True:
+    global running
+    while running:
         msg = conn.recv(1024).decode()
-        print(msg, end="\n")
-
+        print(msg, end = "\n")
 
 s = socket.socket()
 port = input("Server Port: ")
 hs = input("Server Host Name: ")
 us = input("Username: ")
 s.connect((hs, int(port)))
-s.send(f"{us}".encode())
-threading.Thread(target=send_msg, args=(s,)).start()
-threading.Thread(target=wait_msg, args=(s,)).start()
+s.send(f"!j {us}".encode())
+threading.Thread(target = send_msg, args = (s,)).start()
+threading.Thread(target = wait_msg, args = (s,)).start()
